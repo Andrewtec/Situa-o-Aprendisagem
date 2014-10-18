19088f45edf6965fc13287308a271028747eb5cf
Andre <Andre@Vanusa-PC>
2014-10-17 23:46:44 -0300

    Inclusão Classe Jpa Filter

--- src/projeto/util/JpaUtil.java
+++ src/projeto/util/JpaUtil.java
@@ -1,23 +1,41 @@
 package projeto.util;
 
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
 public class JpaUtil {
 
+	private static final String ENTITY_MANAGER = "ENTITY_MANAGER";
 	private static EntityManagerFactory entityManagerFactory;
 
 	public static void iniciarPersistenceUnit() {
 		if (entityManagerFactory == null)
-			entityManagerFactory = Persistence.createEntityManagerFactory("control_exped_pu");
+			entityManagerFactory = Persistence
+					.createEntityManagerFactory("control_exped_pu");
 	}
 	
-	public static EntityManager getEntityManager() {
+	static EntityManager createEntityManager() {
 		return entityManagerFactory.createEntityManager();
 	}
 
 	public static void fecharPersistenceUnit() {
 		entityManagerFactory.close();		
+	}
+	
+	public static EntityManager getEntityManager() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+		EntityManager entityManager = (EntityManager) request.getAttribute(ENTITY_MANAGER);
+		return entityManager;
+	}
+
+	public static void setEntityManager(ServletRequest request,
+			EntityManager entityManager) {
+		request.setAttribute(ENTITY_MANAGER, entityManager);
+		
 	}
 }
